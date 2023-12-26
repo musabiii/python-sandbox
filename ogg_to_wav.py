efe2a6568321e7892b8370c120f35b8d536238a2
THINKPAD-MUSABI\Musabii <musabii@live.com>
2023-12-26 23:29:44 +0500

    python_sandbox

--- ogg_to_wav.py
+++ ogg_to_wav.py
@@ -1,10 +1,13 @@
 import soundfile as sf
 import os
-import numpy as np
+# import numpy as np
 
 path = os.path.abspath("./");
-file_path = os.path.join(path,'new_file_ru.ogg')
-file_write = os.path.join(path,'new_file_ru_ogg.wav')
+file_path = os.path.join(path,'files/new_file_ru.ogg')
+file_write = os.path.join(path,'files/new_file_ru_ogg.wav')
 
-data, samplerate = sf.read('new_file_ru.ogg')
-sf.write('new_file_ru_ogg.wav', data, samplerate)
+data, samplerate = sf.read(file_path)
+sf.write(file_write, data, samplerate)
+
+# data, samplerate = sf.read('./files/new_file_ru.ogg')
+# sf.write('./files/new_file_ru_ogg.wav', data, samplerate)
--- sr_SpeechRecognition.py
+++ sr_SpeechRecognition.py
@@ -1,6 +1,8 @@
 # importing libraries
 import speech_recognition as sr
 import os
+import soundfile as sf
+
 
 # create a speech recognition object
 r = sr.Recognizer()
@@ -11,8 +13,22 @@ def transcribe_audio(path):
         text = r.recognize_google(audio_listened, language='ru-RU')
     return text
 
-path = os.path.abspath("./");
-file_path = os.path.join(path,"new_file_ru.wav")
-print(path);
-txt = transcribe_audio(file_path)
+def ogg_to_wav(ogg_file_path):
+    data, samplerate = sf.read(ogg_file_path)
+    wav_file_path = ogg_file_path + ".wav"
+    sf.write(wav_file_path, data, samplerate)
+    return wav_file_path
+
+file_name = "files/new_file_ru.ogg"
+
+workdir = os.path.abspath("./");
+ogg_file_path = os.path.join(workdir, file_name)
+
+wav_file_path = ogg_to_wav(ogg_file_path)
+
+txt = transcribe_audio(wav_file_path)
+
+if os.path.exists(wav_file_path):
+    os.remove(wav_file_path)
+
 print(txt);
